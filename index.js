90603c67ce623d1753520acdb9f53c8d4bbdefcf
AlinaKa7 <7k.alina32@gmail.com>
2023-04-28 10:03:41 +0300

    hometask 5

---
+++ index.js
@@ -0,0 +1,5 @@
+firstNumber = +prompt('Please enter your first number.');
+secondNumber = +prompt('Please enter your second number.');
+thirdNumber = +prompt('Please enter your third number.');
+average = (firstNumber + secondNumber + thirdNumber) / 3;
+alert(`Average number is ${average}`);
